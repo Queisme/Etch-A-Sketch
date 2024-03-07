503e255c0590adecf7ed800348f4771adf6b5b05
Queisme <queiskey@gmail.com>
2024-03-06 19:11:06 -0600

    update

--- script.js
+++ script.js
@@ -2,27 +2,37 @@ const body = document.querySelector('body');
 const header1 = document.querySelector('h1');
 const main = document.querySelector('.main');
 const container = document.createElement('div');
-const button = document.createElement('button');
+const clear = document.createElement('button');
+const userInput = prompt('Please enter a number: 1 - 100');
+
 body.appendChild(header1);
 body.appendChild(main);
 body.appendChild(container);
-body.appendChild(button);
-body.insertBefore(button, main);
+body.appendChild(clear);
+body.insertBefore(clear, main);
 container.classList.add('container');
-button.classList.add('magic');
-button.textContent = 'Magic';
-container.style.height = '400px';
-container.style.width = '400px';
-container.style.border = '3px solid black';
+clear.classList.add('clear');
+clear.textContent = 'Clear';
+clear.addEventListener('click', reload);
+
+container.style.height = '960px';
+container.style.width = '960px';
 container.style.display = 'flex';
-container.style.justifyContent = 'space-evenly';
+container.style.justifyContent = 'space-between';
 container.style.alignItems = 'center';
 container.style.flexWrap = 'wrap';
+check(userInput);
 
-// 400/userInput = height/width (minus a px for the borders)
+function check(userInput) {
+    if (userInput > 100) {
+        alert ("I just can't do it, Captain! I don't have the power!");
+    } else {
+        makeCells(userInput);
+    };
+};
 
-function makeCells(userInput = 16) {
-    const cellSize = (400 / userInput) - 1;
+function makeCells() {
+    const cellSize = Math.floor(960 / userInput) - 1;
     for (let i = 0; i < userInput; i++) {
         for (let j = 0; j < userInput; j++) {
             const cells = document.createElement('div');
@@ -36,6 +46,6 @@ function makeCells(userInput = 16) {
     };
 };
 
-makeCells(10);
-
-
+function reload() {
+    window.location.reload();
+};
